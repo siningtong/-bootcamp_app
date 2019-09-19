66110db80cbe8df6092330cd44894e230af1300a
siningtong <siningtong@gmail.com>
2019-09-19 03:43:51 +0000

    injection protection

--- teachers.js
+++ teachers.js
@@ -6,16 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
+const cohortName = process.argv[2];
+const value = [`%${cohortName}%`]
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name like $1
 ORDER BY teacher;
-`)
+`,value)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
